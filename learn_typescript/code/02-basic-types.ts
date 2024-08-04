144153417bd2e26b8d9a545ba74ba30b161d0b18
cheeseburgertony <1821955020@qq.com>
2024-08-04 15:38:43 +0800

    基础类型

---
+++ learn_typescript/code/02-basic-types.ts
@@ -0,0 +1,16 @@
+let isDone: boolean = false
+
+let age: number = 18
+
+let firstName: string = 'tony'
+let message: string = `Hello I'm${firstName}, age is ${age}`
+
+let u: undefined = undefined
+let n: null = null
+
+// 不能将类型“undefined”分配给其他类型
+// let num: number = undefined
+
+// 不能将类型“null”分配给其他类型
+// let num: number = null
+
