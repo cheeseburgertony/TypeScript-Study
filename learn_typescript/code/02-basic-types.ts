2d82d5955c337c940531ed0db02741b050f8517b
cheeseburgertony <1821955020@qq.com>
2024-08-04 15:39:04 +0800

    any类型和联合类型

--- learn_typescript/code/02-basic-types.ts
+++ learn_typescript/code/02-basic-types.ts
@@ -14,3 +14,19 @@ let n: null = null
 // 不能将类型“null”分配给其他类型
 // let num: number = null
 
+// any类型(在明确类型的情况下不推荐使用)
+let notSure: any
+notSure = 2
+notSure = 'string'
+notSure = true
+notSure.name
+notSure.getName()
+
+// 联合类型  可以是多种类型(由自己规定可以是那些类型)
+let numberOrString: number | string
+numberOrString = 123
+numberOrString = 'string'
+// numberOrString = true  只能是number或则string类型
+
+
+
